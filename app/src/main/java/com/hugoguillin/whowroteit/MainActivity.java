1081e5f0d3db22a5d2015dadf63c4d0a7f0ac6c2
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-16 16:53:53 +0200

    Task_3

--- app/src/main/java/com/hugoguillin/whowroteit/MainActivity.java
+++ app/src/main/java/com/hugoguillin/whowroteit/MainActivity.java
@@ -1,8 +1,12 @@
 package com.hugoguillin.whowroteit;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchBooks(View view) {
         String libroBuscado = busqueda.getText().toString();
-        new FetchBook(titulo, autor).execute(libroBuscado);
+        InputMethodManager inputMethodManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(inputMethodManager != null){
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        ConnectivityManager manager = (ConnectivityManager)
+            getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(manager != null){
+            networkInfo = manager.getActiveNetworkInfo();
+        }
+        if(networkInfo != null && networkInfo.isConnected()
+            && busqueda.length()!=0) {
+
+            new FetchBook(titulo, autor).execute(libroBuscado);
+            autor.setText("");
+            titulo.setText(R.string.loading);
+        }else{
+            if(busqueda.length() == 0){
+                autor.setText("");
+                titulo.setText(getString(R.string.no_search_term));
+            }else{
+                autor.setText("");
+                titulo.setText(getString(R.string.sin_conexion));
+            }
+        }
     }
 }
